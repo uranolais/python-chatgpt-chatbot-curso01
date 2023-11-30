bdc373a82c2540446eb3b9bf894db6de95ab66fc
Paulo Patto <paulopatto@paulopatto.com>
2023-11-30 12:20:02 -0300

    Update integracao_api.py
    - Update code to fit OpenAI lib 1.x
    - Add error handling to RateLimit and improvem error mesage in pt-br

--- integracao_api.py
+++ integracao_api.py
@@ -1,21 +1,24 @@
-import openai
+from openai import OpenAI, RateLimitError
 import dotenv
 import os
 
 dotenv.load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
-resposta = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {
-            "role": "system",
-            "content": "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
-        },
-        {
-            "role": "user",
-            "content": "Gere 5 produtos"
-        } 
-    ]
-)
-print(resposta)
+client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
+try:
+    resposta = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = [
+            {
+                "role": "system",
+                "content": "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
+            },
+            {
+                "role": "user",
+                "content": "Gere 5 produtos"
+            } 
+        ]
+    )
+    print(resposta)
+except RateLimitError as err:
+    print("Limite de requisições a API OpenAI alcançado, verifique seus créditos em: https://platform.openai.com/account/billing/overview")
